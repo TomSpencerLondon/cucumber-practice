87d1de4623cfc9fbaaec75afe84f41525904bb36
Tom <thomas.spencer@codurance.com>
2021-04-03 17:32:23 +0100

    add code for step definitions

--- step_definitions/CheckoutSteps.java
+++ step_definitions/CheckoutSteps.java
@@ -2,24 +2,27 @@ package step_definitions;
 
 import io.cucumber.java.en.*;
 import io.cucumber.java.PendingException;
+import implementation.Checkout;
+import static org.junit.Assert.*;
 
 public class CheckoutSteps {
-  @Given("the price of a {string} is 40c")
-  public void the_price_of_a_is_40c(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  Checkout checkout;
+  int bananaPrice;
+
+  @Given("^the price of a \"(.*?)\" is (\\d+)c$")
+  public void the_price_of_a_is_40c(String name, int price) throws Throwable {
+      bananaPrice = price;
   }
 
-  @When("I checkout {int} {string}")
-  public void i_checkout(Integer int1, String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  @When("^I checkout (\\d+) \"(.*?)\"$")
+  public void i_checkout(int itemCount, String itemName) throws Throwable {
+    checkout = new Checkout();
+    checkout.add(itemCount, bananaPrice);
   }
 
-  @Then("the total price should be 40c")
-  public void the_total_price_should_be_40c() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  @Then("^the total price should be (\\d+)c$")
+  public void the_total_price_should_be_40c(int total) throws Throwable {
+      assertEquals(total, checkout.total());
   }
 }
 
