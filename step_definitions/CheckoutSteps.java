03bef6ec7573030a5190cc431dc63551eb5d09c5
Tom <thomas.spencer@codurance.com>
2021-04-03 07:21:36 +0100

    add step definition - not implemented

---
+++ step_definitions/CheckoutSteps.java
@@ -0,0 +1,25 @@
+package step_definitions;
+
+import io.cucumber.java.en.*;
+import io.cucumber.java.PendingException;
+
+public class CheckoutSteps {
+  @Given("the price of a {string} is 40c")
+  public void the_price_of_a_is_40c(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("I checkout {int} {string}")
+  public void i_checkout(Integer int1, String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the total price should be 40c")
+  public void the_total_price_should_be_40c() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+}
+
