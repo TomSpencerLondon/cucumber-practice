81a031b6cacb4bde229a3eb8f42719e1bf7093c6
Tom <thomas.spencer@codurance.com>
2021-04-03 18:28:59 +0100

    add passing test

--- features/cucumber.feature
+++ features/cucumber.feature
@@ -1,11 +1,11 @@
 Feature: Checkout
   Scenario: Checkout a banana
-    Given the price of a "banana" is 40c
+    Given the price of 1 "banana" is 40c
     When I checkout 1 "banana"
     Then the total price should be 40c
 
   Scenario Outline: Checkout bananas
-    Given the price of a "banana" is 40c
+    Given the price of 1 "banana" is 40c
     When I checkout <count> "banana"
     Then the total price should be <total>c
 
@@ -15,8 +15,17 @@ Feature: Checkout
     | 2     | 80    |
 
   Scenario: Two bananas scanned separately
-    Given the price of a "banana" is 40c
+    Given the price of 1 "banana" is 40c
     When I checkout 1 "banana"
     And I checkout 1 "banana"
     Then the total price should be 80c
-     		
+  
+  Scenario: A banana and an apple
+    Given the price of 1 "banana" is 40c
+    And the price of 1 "apple" is 25c
+    When I checkout 1 "banana"
+    And I checkout 1 "apple"
+    Then the total price should be 65c
+
+
+
--- step_definitions/CheckoutSteps.java
+++ step_definitions/CheckoutSteps.java
@@ -8,19 +8,28 @@ import static org.junit.Assert.*;
 public class CheckoutSteps {
   Checkout checkout = new Checkout();
   int bananaPrice;
+  int applePrice;
 
-  @Given("^the price of a \"(.*?)\" is (\\d+)c$")
-  public void the_price_of_a_is_40c(String name, int price) throws Throwable {
-      bananaPrice = price;
+  @Given("^the price of 1 \"(.*?)\" is (\\d+)c$")
+  public void the_price_of_1_is_c(String name, int price) throws Throwable {
+      if(name.equals("banana")) {
+        bananaPrice = price;
+      } else if (name.equals("apple")){
+        applePrice = price;
+      }
   }
 
   @When("^I checkout (\\d+) \"(.*?)\"$")
   public void i_checkout(int itemCount, String itemName) throws Throwable {
-    checkout.add(itemCount, bananaPrice);
+    if (itemName.equals("banana")) {
+      checkout.add(itemCount, bananaPrice);
+    } else if (itemName.equals("apple")) {
+      checkout.add(itemCount, applePrice);
+    }
   }
   
   @Then("^the total price should be (\\d+)c$")
-  public void the_total_price_should_be_40c(int total) throws Throwable {
+  public void the_total_price_should_be_c(int total) throws Throwable {
       assertEquals(total, checkout.total());
   }
 }
